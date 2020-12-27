86a8c029eed2e3f4251c6a5d699d0377435b6ff8
gauravchauhan1 <32068255+gauravchauhan1@users.noreply.github.com>
2020-12-27 16:57:01 +0530

    Update intro_1.sol

--- intro_1.sol
+++ intro_1.sol
@@ -16,3 +16,32 @@ contract MyContract{
         value= _value;
 }
 }
+
+///using struts and mapping for data storing
+
+/*
+pragma solidity 0.8.0;
+
+
+
+contract MyContract{
+
+mapping(uint => Person) public people;
+uint256 public peopleCount=0;
+  struct Person{
+      uint _id;
+      string _firstName;
+      string _lastName;
+  }
+  
+  function addPerson(string memory _firstName,string memory _lastName) public{
+      
+      peopleCount +=1;
+      people[peopleCount]= Person(peopleCount,_firstName,_lastName);
+  }
+    
+    
+ 
+}
+
+*/
