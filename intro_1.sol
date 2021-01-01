650174bcfd6ea721b82c702ecc85fcab4c3085f4
gauravchauhan1 <32068255+gauravchauhan1@users.noreply.github.com>
2021-01-01 17:13:41 +0530

    Update intro_1.sol

--- intro_1.sol
+++ intro_1.sol
@@ -166,3 +166,44 @@ contract MyContract{
 }
  */
  
+ ///contracts calling functions from other contract
+ 
+ /*
+  pragma solidity 0.5.1;
+
+contract ERC20Token{
+    string public name;
+  mapping(address =>uint256) public balances;
+  function mint() public{
+      balances[tx.origin] ++;
+  }
+}
+
+contract MyContract{
+   
+    address payable wallet;
+    address public token;
+   
+    constructor(address payable _wallet, address _token) public{
+        wallet = _wallet;
+     token= _token;
+    }
+    
+    //payable is needed in function for ether transcations 
+    
+    function() external payable{
+        buyToken();
+        //external can only be called outside
+    }
+    function buyToken() public payable{
+        ERC20Token _token= ERC20Token(address(token));
+        _token.mint();
+        wallet.transfer(msg.value);
+      
+        
+        
+    }
+ 
+}
+*/
+
